b432db5809beb23196d0f49bc19798bbf1541b79
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-08-17 22:47:41 +0530

    update the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Routes,Route} from 'react-router-dom';
+import AddSong from './pages/AddSong';
+import AddAlbum from './pages/AddAlbum';
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='flex items-start min-h-screen'>
+      <ToastContainer/>
+
+      <div className='flex-1 h-screen overflow-y-scroll bg-[#F3FFF7]'>
+        <div className='pt-8 pl-5 sm:pt-2 sm:pl-12'>
+      <Routes>
+        <Route path='/add-song' element={<AddSong/>}/>
+        <Route path='/add-album' element={<AddAlbum/>}/>
+      </Routes>
+        </div>
+      </div>
     </div>
   )
 }
